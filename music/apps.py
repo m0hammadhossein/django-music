ec57618939333b36a33bf3f7cc311ce6d8e24162
m0hammadhossein <mohammadhossein.mail8731@gmail.com>
2022-03-02 13:44:07 +0330

    add signal comment

--- music/apps.py
+++ music/apps.py
@@ -5,3 +5,6 @@ class MusicConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'music'
     verbose_name = 'موزیک'
+
+    def ready(self):
+        import music.signals
